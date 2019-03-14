9976b3678bbafff2446590f19e9764148d2eca99
Lauren N. Roth <laurennicoleroth@gmail.com>
2019-03-14 14:23:04 -0400

    Add create

---
+++ Wanderlist/Controller/CreateWanderlistViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CreateWanderlistViewController.swift
+//  Wanderlist
+//
+//  Created by Lauren Nicole Roth on 3/14/19.
+//  Copyright © 2019 Lauren Nicole Roth. All rights reserved.
+//
+
+import UIKit
+
+class CreateWanderlistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+ 
+
+}
